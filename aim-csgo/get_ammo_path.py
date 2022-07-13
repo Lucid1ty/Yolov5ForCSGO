37d36f06b48e49276f12323e0fa63ec0a402ebf9
Lucidity <2993514952@qq.com>
2022-07-13 21:14:33 +0800

    change chinese comment to english

--- aim-csgo/get_ammo_path.py
+++ aim-csgo/get_ammo_path.py
@@ -1,11 +1,10 @@
 # _*_ coding : utf-8 _*_
 # @Time : 2022/6/30 14:13
-# @Author : Cosmica
+# @Author : Lucid1ty
 # @File : get_ammo_path
 # @Project : Yolov5ForCSGO
 
-
-# 未完成
+# Incomplete
 import csv
 
 f = csv.reader(open('./ammo_path/ak47.csv', encoding='utf-8'))
@@ -14,29 +13,3 @@ for i in f:
     list.append(i)
 
 print(list)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
